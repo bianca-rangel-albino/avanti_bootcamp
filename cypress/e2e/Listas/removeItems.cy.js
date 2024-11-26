f5ecfacfb6795309c57811cf68b3b259a2bf9347
Anderson Silva <andersonsilv.mota@gmail.com>
2024-11-25 21:56:46 -0300

    automação de remover itens da lista

---
+++ cypress/e2e/Listas/removeItems.cy.js
@@ -0,0 +1,24 @@
+describe('template spec', () => {
+  it('passes', () => {
+
+    cy.visit('https://www.themoviedb.org')
+
+    cy.get('a[aria-label="Entrar"]').click();
+    cy.get('#username').type('archiefraga');
+    cy.get('#password').type('qwe123');
+    cy.get('#login_button').click();
+    
+    cy.get('.avatar.background_color.pink').click();
+    cy.get('a').contains('Listas').click();
+
+    cy.get('a').contains('Minha Lista - Squad 5').click();
+    cy.get('a').contains('Editar').click();
+
+    cy.get('a').contains('Adicionar/Editar itens').click();
+       
+    cy.contains('h4', 'Capitão América: Guerra Civil') 
+      .parents('li') 
+      .find('span.circle-remove')
+      .click();
+  })
+})
