c1371a36a03204f162e6e662a4821d5ce047c02d
Anderson Silva <andersonsilv.mota@gmail.com>
2024-11-25 21:43:26 -0300

    feat: automação de add items na lista

---
+++ cypress/e2e/Listas/addItems.cy.js
@@ -0,0 +1,25 @@
+describe('template spec', () => {
+  it('passes', () => {
+
+    cy.visit('https://www.themoviedb.org')
+
+    cy.get('a[aria-label="Entrar"]').click();
+    cy.get('#username').type('archiefraga');
+    cy.get('#password').type('qwe123');
+    cy.get('#login_button').click();
+    
+    cy.get('.avatar.background_color.pink').click();
+    cy.get('a').contains('Listas').click();
+
+    cy.get('a').contains('Minha Lista - Squad 5').click();
+    cy.get('a').contains('Editar').click();
+
+    cy.get('a').contains('Adicionar/Editar itens').click();
+    
+    cy.get('#list_item_search').type('Capitão América: Guerra Civil');
+    cy.get('.submit_comment')
+      .should('be.visible') 
+      .should('not.be.disabled') 
+      .click();
+  })
+})
